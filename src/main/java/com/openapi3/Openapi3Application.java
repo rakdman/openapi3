d549576dcb8db3e6b6078066cca71f005c8d4aff
INFORM\rkumar <rakesh.kumar@inform-software.com>
2022-10-09 21:17:25 +0200

    Initial Commit-Working code

---
+++ src/main/java/com/openapi3/Openapi3Application.java
@@ -0,0 +1,13 @@
+package com.openapi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openapi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Openapi3Application.class, args);
+	}
+
+}
